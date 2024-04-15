304458076015527b530ace6ff6fddfb6202984fa
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 23:44:37 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,17 +38,17 @@ def login():
         conn.close()
 
     if user_record and bcrypt.check_password_hash(user_record['mot_de_passe'], password):
-        session['user_id'] = username  # Example of setting session
-        return redirect(url_for('dashboard'))
+        session['user_id'] = username  # Setting session after successful login
+        return redirect(url_for('calendar'))  # Redirect to the calendar page
     else:
         flash('Nom d\'utilisateur ou mot de passe incorrect.', 'error')
         return redirect(url_for('index'))
 
-@app.route('/dashboard')
-def dashboard():
+@app.route('/calendar')
+def calendar():
     if 'user_id' not in session:
-        return redirect(url_for('index'))
-    return 'Bienvenue dans votre tableau de bord, {}'.format(session['user_id'])
+        return redirect(url_for('index'))  # Redirect to login if not logged in
+    return render_template('event_calendar/calendar.html', username=session['user_id'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
