95ef2dabb219af590e21b0800c375f10752b2b3d
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 22:59:21 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -36,5 +36,4 @@ def dashboard():
     return 'Bienvenue dans votre tableau de bord!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
