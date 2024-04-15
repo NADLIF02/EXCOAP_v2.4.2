0b129eadee69b220528c252992cb012f81c2dadd
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 22:16:44 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
+# Initialisation de l'extension de la base de données
+db = SQLAlchemy()
+
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'votre_cle_secrete_ici'
+    app.config.from_object(config_class)  # Charger la configuration depuis une classe de configuration
 
+    # Initialisation des extensions
+    db.init_app(app)
+
+    # Importation des Blueprints
     from app.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from app.event_calendar import calendar as calendar_blueprint
     app.register_blueprint(calendar_blueprint)
+
+    # Vous pouvez ajouter ici d'autres initialisations ou configurations d'extensions
 
     return app
