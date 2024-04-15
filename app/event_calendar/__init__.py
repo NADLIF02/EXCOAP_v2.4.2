a1763dd47ba24ac4c3814fa604b82fe4d19fd31e
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 22:11:32 +0200

    Update __init__.py

--- app/event_calendar/__init__.py
+++ app/event_calendar/__init__.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from .views import calendar
 
-
-calendar = Blueprint('calendar', __name__)
+calendar = Blueprint('calendar', __name__, url_prefix='/calendar')
 
 from . import views
