caf4720d215d4e1c7a36195230af8fd963ab9458
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 21:53:49 +0200

    Update views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,18 +1,13 @@
+# auth/views.py
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from .forms import LoginForm
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        if username in USERS and check_password_hash(USERS[username], password):  # Utilisation de hachage
-            session['logged_in'] = True
-            flash('Vous êtes maintenant connecté!', 'success')
-            return redirect(url_for('calendar.display'))  # Correction ici
-        else:
-            flash('Identifiant ou mot de passe incorrect!', 'error')
+        # Logic here for authentication
+        return redirect(url_for('calendar.display'))
     return render_template('auth/login.html', form=form)
