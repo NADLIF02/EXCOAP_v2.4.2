71d730cb2d3d38f93b1758d87e81d27a3b374aec
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 17:17:44 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-from event_calendar.forms import EventForm
-# Import your data models here, for example
-# from yourapp.models import Event
+from event_calendar.forms import EventForm  # Correct import
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -22,18 +20,6 @@ def login():
         session['logged_in'] = True
         return redirect(url_for('home'))
     return render_template('auth/login.html')
-
-@app.route('/get-events', methods=['GET'])
-def get_events():
-    # Here you should fetch your event data
-    # For example, you might do something like:
-    # events = Event.query.all()
-    # Then convert it to the format FullCalendar expects:
-    event_list = [
-        {'title': 'Alice - Leave', 'start': '2024-04-15', 'end': '2024-04-18'},
-        {'title': 'Bob - Leave', 'start': '2024-04-20', 'end': '2024-04-22'}
-    ]
-    return jsonify(event_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
