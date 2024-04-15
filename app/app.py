905a68cc87e3b259cd57582712eb94c852d383f6
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 23:37:04 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 from flask_bcrypt import Bcrypt
 import psycopg2
 import psycopg2.extras
@@ -9,15 +9,13 @@ bcrypt = Bcrypt(app)
 
 def get_db_connection():
     try:
-        conn = psycopg2.connect("dbname=nom_de_votre_base_de_donnees user=utilisateur password=mot_de_passe")
-        return conn
+        return psycopg2.connect("dbname=nom_de_votre_base_de_donnees user=utilisateur password=mot_de_passe")
     except psycopg2.Error as e:
         print("Unable to connect to the database: ", str(e))
         return None
 
 @app.route('/')
 def index():
-    # Notez le chemin mis à jour ici pour le template
     return render_template('auth/login.html')
 
 @app.route('/login', methods=['POST'])
@@ -25,21 +23,22 @@ def login():
     username = request.form['username']
     password = request.form['password']
     conn = get_db_connection()
-    if conn is None:
+    if not conn:
         flash('Database connection error.', 'error')
         return redirect(url_for('index'))
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
     try:
-        cur.execute("SELECT mot_de_passe FROM utilisateurs WHERE nom_utilisateur = %s", (username,))
-        user_record = cur.fetchone()
-        cur.close()
-        conn.close()
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute("SELECT mot_de_passe FROM utilisateurs WHERE nom_utilisateur = %s", (username,))
+            user_record = cur.fetchone()
     except psycopg2.Error as e:
         flash('An error occurred while fetching user data.', 'error')
         print("Query failed: ", str(e))
-        return redirect(url_for('index'))
+    finally:
+        conn.close()
 
     if user_record and bcrypt.check_password_hash(user_record['mot_de_passe'], password):
+        session['user_id'] = username  # Example of setting session
         return redirect(url_for('dashboard'))
     else:
         flash('Nom d\'utilisateur ou mot de passe incorrect.', 'error')
@@ -47,7 +46,9 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return 'Bienvenue dans votre tableau de bord!'
+    if 'user_id' not in session:
+        return redirect(url_for('index'))
+    return 'Bienvenue dans votre tableau de bord, {}'.format(session['user_id'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
