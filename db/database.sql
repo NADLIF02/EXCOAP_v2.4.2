1594608d4eae5b7ec7c01365c9f6c084c77fa035
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 22:31:18 +0200

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -14,3 +14,12 @@ INSERT INTO utilisateurs (type, mot_de_passe) VALUES ('fer5rfr', 'password123');
 INSERT INTO utilisateurs (type, mot_de_passe) VALUES ('fer5rfr', 'password123');
 INSERT INTO utilisateurs (type, mot_de_passe) VALUES ('fer5rfr', 'password123');
 
+CREATE TABLE evenements (
+    id SERIAL PRIMARY KEY,
+    nom_utilisateur VARCHAR(255) NOT NULL,
+    description TEXT,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    couleur VARCHAR(255) DEFAULT '#ff9f89'
+);
+
