23f7f3945269c435720ec18e7ba5b5ddc47b2a09
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-16 20:24:26 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -66,7 +66,7 @@ def get_absences():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('event_calendar/calendar.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
