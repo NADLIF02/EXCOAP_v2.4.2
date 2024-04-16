aa5343622ffb8d07b2f1498e0a5357fef49b8f82
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-16 22:49:58 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Middleware to parse JSON and urlencoded form data
 app.use(bodyParser.json());
